a12a8c35f8a05642dbf233800a09539483b0d4b4
Jordon <jordon.e.smith@gmail.com>
2022-01-03 23:12:59 -0500

    fix indents

--- ios/Share/ShareViewController.swift
+++ ios/Share/ShareViewController.swift
@@ -11,69 +11,73 @@ import LinkPresentation
 
 class ShareViewController: SLComposeServiceViewController {
 
-    private var isValidMusicUrl: Bool = false
-    private static let validHosts = ["open.spotify.com", "music.apple.com"]
-    private var authToken: String? = nil
-    private var userId: String? = nil
-    private var songLink: String? = nil
+  private var isValidMusicUrl: Bool = false
+  private static let validHosts = ["open.spotify.com", "music.apple.com"]
+  private var authToken: String? = nil
+  private var userId: String? = nil
+  private var songLink: String? = nil
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        let defaults = UserDefaults(suiteName: "group.bolero")
-        authToken = defaults?.string(forKey: "access_token") ?? nil
-        userId = defaults?.string(forKey: "user_id") ?? nil
+  override func viewDidLoad() {
+      super.viewDidLoad()
+      let defaults = UserDefaults(suiteName: "group.bolero")
+      authToken = defaults?.string(forKey: "access_token") ?? nil
+      userId = defaults?.string(forKey: "user_id") ?? nil
 
-        if authToken == nil || authToken == "" {
-          self.textView.text = "Please sign in through the Bolero app :)"
-          self.textView.isEditable = false
-        } else {
-          self.title = "Share song!"
-          self.handleSharedLink()
-        }
-    }
+      if authToken == nil || authToken == "" {
+        self.textView.text = "Please sign in through the Bolero app :)"
+        self.textView.isEditable = false
+      } else {
+        self.title = "Share song!"
+        self.handleSharedLink()
+      }
+  }
   
-    override func isContentValid() -> Bool {
-        // Do validation of contentText and/or NSExtensionContext attachments here
-        print("isContentValid -> " + String(isValidMusicUrl))
-        return isValidMusicUrl
-    }
+  override func isContentValid() -> Bool {
+      // Do validation of contentText and/or NSExtensionContext attachments here
+      print("isContentValid -> " + String(isValidMusicUrl))
+      return isValidMusicUrl
+  }
 
-    override func didSelectPost() {
-        // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
-        if let song = self.songLink {
-          self.postSongLink(song, self.textView.text)
-        }
-        // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
-        self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
-    }
+  override func didSelectPost() {
+      // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
+      if let song = self.songLink {
+        self.postSongLink(song, self.textView.text)
+      }
+      // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
+      self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+  }
 
-    override func configurationItems() -> [Any]! {
-        // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
-        return []
-    }
+  override func configurationItems() -> [Any]! {
+      // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
+      return []
+  }
   
-    private func handleSharedLink() {
-      print("handleSharedLink() -> start")
-      let sharedData = self.extensionContext?.inputItems.first as? NSExtensionItem
-      let provider = sharedData?.attachments?.first
-      if provider?.hasItemConformingToTypeIdentifier("public.url") != nil {
-        Task {
-          let result = try await provider?.loadItem(forTypeIdentifier: "public.url", options: nil)
-          guard result != nil else { return }
-          if let url = result as? URL, let host = url.host {
-            if ShareViewController.validHosts.contains(host) {
-              self.isValidMusicUrl = true
-              self.songLink = url.absoluteString
-              self.fetchPreview(for: url.absoluteString)
-              self.validateContent()
-            } else {
-              self.textView.text = "Why not try a Spotify or Music link?"
-              self.textView.isEditable = false
-            }
+  private func loadCollections() {
+    
+  }
+  
+  private func handleSharedLink() {
+    print("handleSharedLink() -> start")
+    let sharedData = self.extensionContext?.inputItems.first as? NSExtensionItem
+    let provider = sharedData?.attachments?.first
+    if provider?.hasItemConformingToTypeIdentifier("public.url") != nil {
+      Task {
+        let result = try await provider?.loadItem(forTypeIdentifier: "public.url", options: nil)
+        guard result != nil else { return }
+        if let url = result as? URL, let host = url.host {
+          if ShareViewController.validHosts.contains(host) {
+            self.isValidMusicUrl = true
+            self.songLink = url.absoluteString
+            self.fetchPreview(for: url.absoluteString)
+            self.validateContent()
+          } else {
+            self.textView.text = "Why not try a Spotify or Music link?"
+            self.textView.isEditable = false
           }
         }
       }
     }
+  }
   
   private func fetchPreview(for link: String) {
     print("~~~~~~~\nmade it to fetchPreview with " + link)
