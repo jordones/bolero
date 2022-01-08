410e76cbdbe9ed6d4753d2fc35f59e40448e6f1d
jordon smith <jordon.e.smith@gmail.com>
2022-01-08 01:12:17 -0500

    Create storyboard for extension

---
+++ ios/Share/ShareExtViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ShareExtViewController.swift
+//  Share
+//
+//  Created by jordon smith on 2022-01-07.
+//
+
+import Foundation
+import UIKit
+
+class ShareExtViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
+
+//in your custom view controller you can refer to self.extensionContext to read and complete the share action. Refer to the code in the template ShareViewExtension
