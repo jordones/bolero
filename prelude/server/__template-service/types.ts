06809c464793dc3b2b2c3c1ff80f81d5eddda8eb
jordon smith <jordon.e.smith@gmail.com>
2022-01-22 13:34:18 -0500

    cleanup

--- prelude/server/__template-service/types.ts
+++ prelude/server/__template-service/types.ts
@@ -5,4 +5,3 @@ import Controller from './controller';
 export type Repository = ReturnType<typeof Repository>;
 export type Service = ReturnType<typeof Service>;
 export type Controller = ReturnType<typeof Controller>;
-  
